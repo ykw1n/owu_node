fd049fe6ea5cb1ee15e02a2acf9288f69df60511
ykw1n <64686520+ykw1n@users.noreply.github.com>
2021-08-18 10:18:08 +0300

    fix

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const sort = require('./app.js')
 const path2000 = path.join(__dirname, '2000')
 const path1800 = path.join(__dirname, '1800')
 
-sort.femaleMv(path2000, path1800);
+sort.sortFiles(path2000, path1800);
