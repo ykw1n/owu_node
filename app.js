06b43009873a03078ebe06eade19df454cff2d3f
ykw1n <64686520+ykw1n@users.noreply.github.com>
2021-08-18 18:54:04 +0300

    refact

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 exports.sortFiles = function (pathFrom, pathTo, sortBy) {
     const baseDirFrom = path.join(__dirname, pathFrom);
     const baseDirTo = path.join(__dirname, pathTo);
+    
     fs.readdir(baseDirFrom, (err, files) => {
         files.forEach(file => {
             fullCurrPath = path.join(baseDirFrom, file);
             fullPathTo = path.join(baseDirTo, file);
             let text = fs.readFileSync(path.join(baseDirFrom, file));
+            
             if ((/"male/.test(text.toString()) && sortBy != 'male') || (/"female/.test(text.toString()) && sortBy != 'female')) {
                 fs.rename(fullCurrPath, fullPathTo, function (err) {
                     if (err) throw err
